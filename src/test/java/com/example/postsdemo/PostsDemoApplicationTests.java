b3a78b9b19c2695dfe08fe36e265532e73428ae9
Niegowski Tomasz-PNM386 <Tomasz.Niegowski@motorolasolutions.com>
2021-12-01 00:03:15 +0100

    whole project init

---
+++ src/test/java/com/example/postsdemo/PostsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.postsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostsDemoApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
